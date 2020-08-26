73fa3d80b9ecc83ec61206a2db80f758be96b464
psuen1811 <psuen1811@gmail.com>
2020-08-26 01:15:03 -0700

    Enhanced code performance

--- src/com/pakfortune/model/star/Richman.java
+++ src/com/pakfortune/model/star/Richman.java
@@ -38,7 +38,6 @@ public enum Richman {
     public static List<String> calculate(String input) {
         // 取天干
         String [] arr = input.split("(?!^)");
-        String tempStem = Stem.valueOf(arr[0]).name();
-        return ImmutableList.copyOf(list.get(tempStem));
+        return ImmutableList.copyOf(list.get(Stem.valueOf(arr[0]).name()));
     }
 }
