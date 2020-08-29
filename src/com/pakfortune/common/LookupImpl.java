6e45d13d0d98c7f2508981260f32efd8020be73c
psuen1811 <psuen1811@gmail.com>
2020-08-29 02:29:04 -0700

    Put @Override

--- src/com/pakfortune/common/LookupImpl.java
+++ src/com/pakfortune/common/LookupImpl.java
@@ -4,6 +4,7 @@ import com.google.common.base.Enums;
 
 public class LookupImpl implements LookupInterface {
 
+    @Override
     public <E extends Enum<E>> E getIfPresent(Class<E> clazz, String name) {
         return Enums.getIfPresent(clazz, name).orNull();
     }
